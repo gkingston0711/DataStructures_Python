93f3528c9c0b9bdce93dbac717645edd53d89222
Joey Kingston <48415221+gkingston0711@users.noreply.github.com>
2021-02-07 09:29:36 -0800

    her

---
+++ linkedlist/Node.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self):
+        self.next=None
+        self.id=0
+        self.name=""
+
+    def __init__(self,Name,Id):
+        self.next=None
+        self.id=Id
+        self.name=Name
+
+
+
